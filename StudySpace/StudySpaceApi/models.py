ab6a4126c21e5bf43c41141b61f0117e164ef5e2
macktvz <macktvz@gmail.com>
2021-03-24 12:50:55 -0400

    added name to list of users in db

--- StudySpace/StudySpaceApi/models.py
+++ StudySpace/StudySpaceApi/models.py
@@ -14,6 +14,9 @@ class User(models.Model):
     program = models.CharField(max_length=20)
     picture = models.ImageField(upload_to="user")
 
+    def __str__(self):
+        return self.lastName
+
     
 
 class Friends(models.Model):
