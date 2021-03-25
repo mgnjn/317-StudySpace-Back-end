62b23e7f037ed72747e6bf0f22ce960fe5ea9adf
macktvz <macktvz@gmail.com>
2021-03-24 21:12:56 -0400

    Added function to return interest of user given id as example

--- StudySpace/StudySpaceApi/urls.py
+++ StudySpace/StudySpaceApi/urls.py
@@ -5,6 +5,9 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'Users', views.UserViewSet)
 
+
+
+
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
--- StudySpace/StudySpaceApi/views.py
+++ StudySpace/StudySpaceApi/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .serializers import UserSerializer
 from .models import User
+from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.decorators import action
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('lastName')
     serializer_class = UserSerializer
+
+    @action(detail=False)
+    def interest1(self, request, *args, **kwargs):
+        user_id = request.GET.get('id')
+        user = self.queryset.get(id=user_id)
+        return Response(user.interest1)
