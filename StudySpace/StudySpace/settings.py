8bab03ef886f5cc7b19cc5767ac483a2536e6c23
mgnjn <mamtbal@gmail.com>
2021-04-09 18:49:27 -0400

    Database config for postgresql

--- StudySpace/StudySpace/settings.py
+++ StudySpace/StudySpace/settings.py
@@ -92,13 +92,23 @@ WSGI_APPLICATION = 'StudySpace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+  'default':{
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'studyspace-db',
+    'USER':'postgres',  #db user
+    'PASSWORD': 'CP317team2W21',
+    'HOST': 'localhost',
+    'PORT':'5432'
+  }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
